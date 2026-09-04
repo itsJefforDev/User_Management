0d47707e4779cdbc86a01a6ab74c1b43d1391141
Jeff <jeffersonorozco24@gmail.com>
2026-09-04 18:53:00 -0500

    add usuario entity

--- src/main/java/com/userManagement/businessLayer/dto/UsuarioDTO.java
+++ src/main/java/com/userManagement/businessLayer/dto/UsuarioDTO.java
@@ -1,4 +1,17 @@
 package com.userManagement.businessLayer.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
+    private Long id;
+
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private int edad;
 }
