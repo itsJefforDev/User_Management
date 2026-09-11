3d346e6046fbf8e4d11752f9aa4b2bf90e157dc9
Jeff <jeffersonorozco24@gmail.com>
2026-09-10 19:16:04 -0500

    add usuario entity

--- src/main/java/com/userManagement/persistenceLayer/entity/Usuario.java
+++ src/main/java/com/userManagement/persistenceLayer/entity/Usuario.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class usuario {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
